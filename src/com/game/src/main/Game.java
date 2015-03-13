c8c2b3ef038955e2bcd9ca68f3f5737c3f5433e5
newkj1 <samuel.newcombe@hotmail.com>
2015-03-13 17:02:17 +1100

    Initial Commit

---
+++ src/com/game/src/main/Game.java
@@ -0,0 +1,130 @@
+package com.game.src.main;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+
+/**
+ * Created by Agil on 11/03/15.
+ */
+
+public class Game extends Canvas implements Runnable {
+
+
+    public static final int WIDTH = 320;
+    public static final int HEIGHT = WIDTH / 12 * 9;
+    public static final int SCALE = 2;
+    public final String TITLE = "Little red riding-hood";
+
+    private boolean running = false;
+    private Thread thread;
+
+    private BufferedImage image = new BufferedImage(WIDTH,HEIGHT,BufferedImage.TYPE_INT_RGB);
+
+    private synchronized void start() {
+        if(running)
+            return;
+
+        running = true;
+        thread = new Thread(this);
+        thread.start();
+
+    }
+
+    private synchronized void stop() {
+        if(!running)
+            return;
+
+        running = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.exit(1);
+
+    }
+
+    public void run() {
+        long lastTime = System.nanoTime();
+        final double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        int updates = 0;
+        int frames = 0;
+        long timer = System.currentTimeMillis();
+
+
+        while(running) {
+            long now = System.nanoTime();
+            delta += (now - lastTime) / ns;
+            lastTime = now;
+            if (delta >= 1) {
+                tick();
+                updates++;
+                delta--;
+            }
+            render();
+            frames++;
+
+            if (System.currentTimeMillis() - timer > 1000) {
+                timer += 1000;
+                System.out.println(updates + " Ticks, Fps" + frames);
+                updates = 0;
+                frames = 0;
+
+
+            }
+
+        }
+        stop();
+    }
+
+
+    private void tick(){
+
+
+    }
+
+    private void render(){
+        BufferStrategy bs = this.getBufferStrategy();
+
+        if(bs == null){
+            createBufferStrategy(3);
+            return;
+        }
+
+        Graphics g = bs.getDrawGraphics();
+
+        g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+
+        g.dispose();
+        bs.show();
+
+
+    }
+
+    public static void main(String args[]){
+        Game game = new Game();
+        game.setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+        game.setMaximumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+        game.setMinimumSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+
+
+
+        JFrame frame = new JFrame(game.TITLE);
+        frame.add(game);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+
+        game.start();
+
+
+
+    }
+
+}
